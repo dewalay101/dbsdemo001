f2a692bf0778f02e5f9c8573ebf2e3749413a1b8
dewalay101 <32671326+dewalay101@users.noreply.github.com>
2017-10-12 10:18:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "<h1>Hello world from heroku</h1>"
-echo "<p>Building content in a business class</p>"
+echo "<h1>Hello world from heroku</h1>";
+echo "<p>Building content in a business class</p>";
